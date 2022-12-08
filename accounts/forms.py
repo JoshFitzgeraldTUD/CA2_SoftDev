5f7d24ac39dc349ffb5c70576506d2bab38fa977
Aaron Layton <x00204286@mytudublin.ie>
2022-12-08 10:11:26 +0000

    push

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields +('age','email')
+        fields = UserCreationForm.Meta.fields + ('age','email',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
