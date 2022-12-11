fb811547144b41ed8d6ad8dd88b40604dfd691a1
AaronLayton0 <x00204286@mytudublin.ie>
2022-12-11 20:57:36 +0000

    audi json + password change/reset

--- Car_Project/settings.py
+++ Car_Project/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Car_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('accounts/templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
